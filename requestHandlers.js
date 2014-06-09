073ac45bcdfce767d4ee69471859fb7d3a15d74d
Ray <ray.artisan@gmail.com>
2014-06-09 19:45:07 +0800

    阻塞操作

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,23 @@
 function start() {
   console.log("Request handler 'start' was called.");
 
-}
-function upload() {
-  console.log("Request handler 'upload' was called.");
+  function sleep(milliSeconds) {
+    var startTime = new Date().getTime();
+    while(new Date().getTime() < startTime + milliSeconds);
+
+  }
+
+  sleep(10000)
+
+  return "hello start";
 
 }
+
+function upload() {
+  console.log("Request handler 'upload' was called.");
+  return "upload";
+
+}
+
 exports.start = start;
 exports.upload = upload;
